93539f3776f2f64bfc168c079aeb24922cb34ede
Aouadi Slim <slim7040@hotmail.fr>
2018-10-27 23:10:20 +0100

    Added Mail API on add ResourceRequest

--- InfinityMAP-ejb/src/main/java/tn/esprit/PIDEVMap/services/ResourceRequestService.java
+++ InfinityMAP-ejb/src/main/java/tn/esprit/PIDEVMap/services/ResourceRequestService.java
@@ -7,6 +7,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.ejb.Stateful;
@@ -14,6 +15,13 @@ import javax.ejb.Stateless;
 import javax.enterprise.context.RequestScoped;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -50,6 +58,29 @@ public class ResourceRequestService implements ResourceRequestServiceLocal {
        ressourceRequest.setMandateEndDate(date);
        ressourceRequest.setMandateStartDate(date);
 		em.persist(ressourceRequest);
+		final String username = "slim.aouadi@esprit.tn";
+		final String password = "slim@5ra21156990";
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("slim.aouadi@esprit.tn"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("slim.aouadi@esprit.tn"));
+			message.setSubject("");
+			message.setText("ResourceRequest Added");
+			Transport.send(message);
+			System.out.println("Done");
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 		return " Successfull";
 	}
 
