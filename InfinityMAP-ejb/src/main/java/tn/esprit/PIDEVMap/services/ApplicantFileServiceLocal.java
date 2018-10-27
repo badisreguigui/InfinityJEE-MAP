a708e26583ced37717e08d098814dd6113b4386e
FediBn <fedi.bennejma@esprit.tn>
2018-10-27 13:46:24 +0100

    Plusieurs fonctions + ajout services Locales fixes

--- InfinityMAP-ejb/src/main/java/tn/esprit/PIDEVMap/services/ApplicantFileServiceLocal.java
+++ InfinityMAP-ejb/src/main/java/tn/esprit/PIDEVMap/services/ApplicantFileServiceLocal.java
@@ -1,19 +1,20 @@
 package tn.esprit.PIDEVMap.services;
 import java.util.List;
 
+import javax.ejb.Local;
 import javax.ejb.Remote;
 
 import tn.esprit.PIDEVMap.persistence.ApplicantFile;
 import tn.esprit.PIDEVMap.persistence.ApplicantRequest;
 import tn.esprit.PIDEVMap.persistence.Question;
 
-@Remote
-public interface ApplicantFileServiceRemote {
+@Local
+public interface ApplicantFileServiceLocal {
 	/////Passer Test
 	public void searchAppropriateTest(int applicantId, String categorie); //y mettre fonction affecter aussi
 	public void affecterTestAapplicant(int testId, int applicantId); 
 	public List<Question> recevoirQuestions(int testId); //pour l'affichage par la suite dans .Net et maintenant pour boucler sur la liste et avoir chaque question
-	public void repondreQuestion(int questionId, String reponse, int applicantId); 
+	public String repondreQuestion(int questionId, String reponse, int applicantId); 
 	////Resultat
 	//public float resultatTest(int applicantId); //boucle sur les questions et update dans l'applicantFile
 	/////lettre d'embauche, voir s'il faut ajouter un fichier donc une classe qui conient filePath et ApplicantId
